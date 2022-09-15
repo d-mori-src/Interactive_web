a4d8dd09724a7570eb678e26f9f11a5105b5b3b0
p000078 <p000078@cpe-172-100-100-20.twcny.res.rr.com>
2022-09-15 15:52:52 +0900

    変更220915

---
+++ svg_animation/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>* { margin: 0; padding: 0; box-sizing: border-box;}</style>
+    <link rel="stylesheet" href="./css/svg.css">
+</head>
+<body>
+    <img src="./img/ico_svg.svg" class="icoSvg" alt="">
+  
+    <script src="./js/desvg.js"></script>
+    <script src="./js/main.js"></script>
+</body>
+</html>
---
+++ svg_animation/js/main.js
@@ -0,0 +1,1 @@
+deSVG('.icoSvg', true);
